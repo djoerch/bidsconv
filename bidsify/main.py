d04b3aeafde4465a9670c0b2015f3d0a3b235f13
danjgale <danjgale@gmail.com>
2019-07-21 22:59:40 -0400

    bug fixes along with install ignores

--- bidsify/main.py
+++ bidsify/main.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import shutil
 import subprocess
+from pathlib import Path
 import numpy as np
 import pandas as pd
 
@@ -12,21 +13,20 @@ def _cli_parser():
     parser.add_argument('-d', type=str, metavar='dicom_dir',
                         help="The path to the directory which contains all "
                              "subjects' dicom images.")
-    parser.add_argument('--ignore', type='+', metavar='ignored_dirs',
+    parser.add_argument('--ignore', nargs='+', type=str, metavar='ignored_dirs',
                         help="Subdirectories in `-d` to ignore.")
     parser.add_argument('-o', type=str, metavar='output_dir',
                         help="The path to the directory which contains all "
                              "subjects' BIDS data.")
     parser.add_argument('-s', type=str, metavar='session',
                         help="Session number.")
-    parser.add_argument('-c', '--config', type=str.lower, metavar='config',
+    parser.add_argument('-c', type=str.lower, metavar='config',
                         help='Configuration .json file for dcm2bids.')
     return parser.parse_args()
 
 
 def _run_dcm2bids(sub_id, config, output_path, dicom_path, session=None):
-    cmd_str = "dcm2bids -p {} -c {} -o {} -d '{}'"
-    cmd_str.format(sub_id, config, output_path, dicom_path)
+    cmd_str = "dcm2bids -p {} -c {} -o {} -d '{}'".format(sub_id, config, output_path, dicom_path)
     if session is not None:
         cmd_str += " -s {}".format(session)
     print(cmd_str)
@@ -36,27 +36,32 @@ def _run_dcm2bids(sub_id, config, output_path, dicom_path, session=None):
 def main():
 
     params = vars(_cli_parser())
+    print(params)
     sub_data = []
-    for i, dirname in os.listdir(params['dicom_dir']):
+    sub_count = 1
+    for dirname in os.listdir(params['d']):
 
-        in_path = os.path.join(params['dicom_dir'], dirname)
-        if dirname in params['ignored_dirs']:
+        in_path = os.path.join(params['d'], dirname)
+        if dirname in params['ignore']:
             print('Skipping {}'.format(in_path))
             continue
         else:
             print('Processing {}'.format(in_path))
 
-        sub_id = 'sub-0{}'.format(i + 1)
-        output_path = os.path.join(params['output_dir'], sub_id)
+        sub_id = 'sub-{}'.format(str(sub_count).zfill(2))
+        sub_count += 1
         sub_data.append([sub_id, dirname])
 
-        _run_dcm2bids(sub_id, params['config'], output_path, in_path,
-                      session=params['session'])
+        _run_dcm2bids(sub_id, params['c'], params['o'], in_path,
+                      session=params['s'])
 
     sub_data = pd.DataFrame(np.array(sub_data),
                             columns=['participant_id', 'dicom_dir'])
-    sub_data.to_csv(os.path.join(params['output_dir'], 'participants.tsv'),
-                    sep=r'\t')
+    sub_data.to_csv(os.path.join(params['o'], 'participants.tsv'),
+                    sep='\t')
+    Path(os.path.join(params['o'], 'README')).touch()
+    Path(os.path.join(params['o'], 'CHANGES')).touch()
+    Path(os.path.join(params['o'], 'dataset_description.json')).touch()
 
 
 

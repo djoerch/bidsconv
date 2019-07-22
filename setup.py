53225c15ded341726ad22ad99d9a5bf85edd6fde
Dan Gale <danjgale@gmail.com>
2019-07-21 21:07:02 -0400

    bare bones setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+test_deps = ['pytest-cov',
+             'pytest']
+
+extras = {
+    'test': test_deps,
+}
+
+setup(
+    name='bidsify',
+    version='0.0.1',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"]),
+    license='MIT',
+    author='Dan Gale',
+    long_description=open('README.md').read(),
+    url='https://github.com/danjgale/bidsify',
+    tests_require=test_deps,
+    extras_require=extras,
+    setup_requires=['pytest-runner'],
+    entry_points={
+        'console_scripts': [
+            'bidsify=bidsify.main:main'
+            ]
+        }
+)
